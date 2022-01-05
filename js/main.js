4bc522c09649619689ce60057f01ad7b493cb3fb
HamnYce <hamadboa@gmail.com>
2022-01-05 06:26:56 +0300

    add autoMove functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // w = 119, a = 97, s = 115, d = 100
+let globalIntervalCounter = 0;
 
 function initScreen() {
   let root = document.getElementById("playground");
@@ -13,51 +14,100 @@ function initScreen() {
   }
 
   window.addEventListener("keypress", moveSnake);
-}
 
+  // Init first head
+  document.getElementById("0-0").classList.add("head")
+}
 
 function moveSnake(e) {
   let code = e.keyCode;
   let head = document.querySelector(".head");
-  let coord = head.id.split('-');
 
-  console.log(code)
-
-  if (code == 119) moveHeadUp(head, coord);// W, up
-  else if (code == 97) moveHeadLeft(head, coord);// A, left
-  else if (code == 115) moveHeadDown(head, coord);// S, down
-  else if (code == 100) moveHeadRight(head, coord);// D, right
+  if (code == 119) {// W, up
+    clearInterval(globalIntervalCounter)
+    globalIntervalCounter = autoMoveUp();
+  }
+  else if (code == 97) {// A, left
+    clearInterval(globalIntervalCounter)
+    globalIntervalCounter = autoMoveLeft();
+  }
+  else if (code == 115) {// S, down
+    clearInterval(globalIntervalCounter)
+    globalIntervalCounter = autoMoveDown();
+  }
+  else if (code == 100) {// D, right
+    clearInterval(globalIntervalCounter)
+    globalIntervalCounter = autoMoveRight();
+  }
 }
-function moveHeadLeft(head, coord) {
+
+function autoMoveLeft() {return setInterval(moveLeftInterval, 250)}
+
+function autoMoveUp() {return setInterval(moveUpInterval, 250)}
+
+function autoMoveRight() {return setInterval(moveRightInterval, 250)}
+
+function autoMoveDown() {return setInterval(moveDownInterval,250)}
+
+function moveLeftInterval() {
+  let head = document.querySelector('.head')
+
+  moveHeadLeft(head)
+}
+
+function moveUpInterval() {
+  let head = document.querySelector('.head')
+
+  moveHeadUp(head)
+}
+
+function moveRightInterval() {
+  let head = document.querySelector('.head')
+
+  moveHeadRight(head)
+}
+
+function moveDownInterval() {
+  let head = document.querySelector('.head')
+
+  moveHeadDown(head)
+}
+
+function moveHeadLeft(head) {
+  let coord = head.id.split('-');
   if (coord[1] == 0) return;
 
-  changeHead(head, coord, [parseInt(coord[0]), parseInt(coord[1]) - 1]);
+  changeHead(head, [parseInt(coord[0]), parseInt(coord[1]) - 1]);
 }
-function moveHeadUp(head, coord) {
+
+function moveHeadUp(head) {
+  let coord = head.id.split('-');
   if (coord[0] == 0) return;
 
-  changeHead(head, coord, [parseInt(coord[0]) - 1, parseInt(coord[1])]);
+  changeHead(head, [parseInt(coord[0]) - 1, parseInt(coord[1])]);
 }
-function moveHeadRight(head, coord) {
+
+function moveHeadRight(head) {
+  let coord = head.id.split('-');
   if (coord[1] == 9) return;
 
-  changeHead(head, coord, [parseInt(coord[0]), parseInt(coord[1]) + 1]);
+  changeHead(head, [parseInt(coord[0]), parseInt(coord[1]) + 1]);
 }
-function moveHeadDown(head, coord) {
+
+function moveHeadDown(head) {
+  let coord = head.id.split('-');
   if (coord[0] == 9) return;
 
-  changeHead(head, coord, [parseInt(coord[0]) + 1, parseInt(coord[1])]);
+  changeHead(head, [parseInt(coord[0]) + 1, parseInt(coord[1])]);
 }
-function changeHead(head, coord, newCoord) {
+
+function changeHead(head, newCoord) {
   head.classList.toggle("head");
   head = document.getElementById(
     `${newCoord[0]}-${newCoord[1]}`
     );
-  head.classList.toggle("head")
+  head.classList.toggle("head");
 }
 
 
-initScreen()
-
-let x = document.getElementById("0-0");
-x.classList.add("head")
+initScreen();
