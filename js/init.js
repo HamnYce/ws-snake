3a89e5ae54a73ab7d3b62c52581020d8f153d118
HamnYce <hamadboa@gmail.com>
2022-01-05 19:32:51 +0300

    change file name, prepare for refactor

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@ let movingLeft = false;
 let movingRight = false;
 let movingUp = false;
 let movingDown = false;
+let snakeBody = ["0-0"]
 
 function initScreen() {
   let root = document.getElementById("playground");
@@ -22,7 +23,7 @@ function initScreen() {
   // Init first head
   let x = Math.floor(Math.random()*10)
   let y = Math.floor(Math.random()*10)
-  document.getElementById(`${x}-${y}`).classList.add("head")
+  document.getElementById(snakeBody[0]).classList.add("head")
 }
 
 function moveSnake(e) {
@@ -102,38 +103,97 @@ function moveDownInterval() {
 function moveHeadLeft(head) {
   let coord = head.id.split('-');
   if (coord[1] == 0) return;
-
-  changeHead(head, [parseInt(coord[0]), parseInt(coord[1]) - 1]);
+  let newCoord = [parseInt(coord[0]), parseInt(coord[1]) - 1]
+  if (isApple(newCoord)) {
+    grow(head, newCoord)
+    removePowerUp()
+  }
+  changeHead(head, newCoord);
 }
 
 function moveHeadUp(head) {
   let coord = head.id.split('-');
   if (coord[0] == 0) return;
+  let newCoord = [parseInt(coord[0]) - 1, parseInt(coord[1])]
+  if (isApple(newCoord)) {
+    grow(head, newCoord)
+    removePowerUp()
+  }
 
-  changeHead(head, [parseInt(coord[0]) - 1, parseInt(coord[1])]);
+  changeHead(head, newCoord);
 }
 
 function moveHeadRight(head) {
   let coord = head.id.split('-');
   if (coord[1] == 9) return;
+  let newCoord = [parseInt(coord[0]), parseInt(coord[1]) + 1];
+  if (isApple(newCoord)) {
+    grow(head, newCoord)
+    removePowerUp()
+  }
 
-  changeHead(head, [parseInt(coord[0]), parseInt(coord[1]) + 1]);
+  changeHead(head, newCoord);
 }
 
 function moveHeadDown(head) {
   let coord = head.id.split('-');
   if (coord[0] == 9) return;
+  let newCoord = [parseInt(coord[0]) + 1, parseInt(coord[1])];
+  if (isApple(newCoord)) {
+    grow(head, newCoord)
+    removePowerUp()
+  }
 
-  changeHead(head, [parseInt(coord[0]) + 1, parseInt(coord[1])]);
+  changeHead(head, newCoord);
 }
 
 function changeHead(head, newCoord) {
-  head.classList.toggle("head");
+  head.classList.remove("head");
   head = document.getElementById(
     `${newCoord[0]}-${newCoord[1]}`
     );
-  head.classList.toggle("head");
+  head.classList.add("head");
+}
+
+function grow(head, newCoord) {
+  head.classList.remove("head")
+  head.classList.remove("snake-segment-empty")
+  head.classList.add("snake-segment-filled")
+  head = document.getElementById(
+    `${newCoord[0]}-${newCoord[1]}`
+    );
+  head.classList.add("head")
+  snakeBody.push(`${newCoord[0]}-${newCoord[1]}`)
+}
+
+function createPowerUp() {
+  let x = Math.floor(Math.random()*10);
+  let y = Math.floor(Math.random()*10);
+  let randomPlace = document.getElementById(`${x}-${y}`);
+  let randomPlaceClassList = randomPlace.classList;
+  while (randomPlaceClassList.contains('snake-segment-filled')) {
+  x = Math.floor(Math.random()*10);
+  y = Math.floor(Math.random()*10);
+  randomPlace = document.getElementById(`${x}-${y}`);
+  randomPlaceClassList = randomPlace.classList;
+  }
+  randomPlace.classList.add('apple')
+}
+
+function removePowerUp() {
+  document.querySelector('.apple').classList.remove('apple')
+}
+
+function isApple(coord) {
+  if (document.getElementById(`${coord[0]}-${coord[1]}`)
+      .classList.contains('apple')) 
+        return true;
+  return false;
 }
 
 
+
+
 initScreen();
+
+createPowerUp();
